d1526e3e47aca0eed7a0b530cf19b6d1fa4fdd57
g0 <59220768+g0-I@users.noreply.github.com>
2021-12-29 22:32:27 +0900

    Update callable.tsx

--- ex/3/03-function-classable/callable.tsx
+++ ex/3/03-function-classable/callable.tsx
@@ -1,2 +1,26 @@
 //callable object type
 
+{
+  interface NumOp {
+    ( n: number, m:number ): number;
+  }
+
+  const add: NumOp = function (n, m){
+    return n + m;
+  };
+  const subtract: NumOp = (n, m) => n - m;
+  
+  console.log(add(1, 2));  // 3
+  console.log(subtract(7, 2));  // 5
+}
+
+// in-line
+{
+  const add: (n: numberm, m: number) => number = function (n, m) {
+    return n + m;
+  };
+  const subtract: (n: number, m: number) => number = (n, m) => n - m;
+
+  console.log(add(3, 7)); // 10
+  console.log(subtract(10, 8)) // 2
+}
